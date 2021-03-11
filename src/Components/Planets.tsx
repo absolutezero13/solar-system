ff317daf9d82c32bb270849381e1e54974e2a6d1
unknown <ilker897@gmail.com>
2021-03-11 19:24:59 +0300

    small adjustments

--- src/Components/Planets.tsx
+++ src/Components/Planets.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import PlanetContext from "../Context/PlanetContext";
 
 const Planets: React.FC = () => {
