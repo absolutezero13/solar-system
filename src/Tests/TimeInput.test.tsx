1f504418da0f1e6aae7a0814132c44e37c28d964
unknown <ilker897@gmail.com>
2021-03-13 09:47:08 +0300

    small fix scrollbar

--- src/Tests/TimeInput.test.tsx
+++ src/Tests/TimeInput.test.tsx
@@ -1,18 +1,13 @@
 import { shallow } from "enzyme";
 import TimeInput from "../Components/TimeInput";
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import React from "react";
 
-// Enzyme.configure({
-//   adapter: new Adapter(),
-// });
+test("should set input value", () => {
+  const wrapper = shallow(<TimeInput />);
+  const value = "235";
+  wrapper.find("input").at(0).simulate("change", {
+    target: value,
+  });
 
-// test("should set input value", () => {
-//   const wrapper = shallow(<TimeInput />);
-//   const value = "235";
-//   wrapper.find("input").at(0).simulate("change", {
-//     target: value,
-//   });
-
-//   expect(wrapper.state("lul")).toBe(value);
-// });
+  expect(wrapper.state("lul")).toBe(value);
+});
